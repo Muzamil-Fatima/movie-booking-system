ed8838b04d95db28c4f50ab64b44528369031f97
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2025-11-08 21:59:03 +0500

    add lucide-react make the navbar simple without importing different picture

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,13 +5,7 @@ import Footer from "../component/Footer";
 export const Home = () => {
   return (
     <>
-      <div>Home</div>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque esse
-        sequi et quibusdam cumque voluptatibus animi? Architecto laboriosam,
-        modi dolorem sed odio sunt suscipit nobis repudiandae esse culpa eum
-        ullam? Explicabo, deserunt officia.
-      </p>
+     
     </>
   );
 };
