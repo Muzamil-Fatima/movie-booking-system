7dd1b91c55e96ee9300e5552788b7bbc027cebc4
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2025-11-09 17:45:14 +0500

    Access Control With the help of Clerk

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUser();
+  const { OpenSignIn } = useClerk();
+
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-center px-6 md:px-16 lg:px-36 py-5">
       <Link to="/" className="max-md:flex-1">
@@ -61,13 +65,21 @@ export const Navbar = () => {
       </div>
       <div className="flex items-center gap-8">
         <SearchIcon className="max-md:hidden w-6 h-6  cursor-pointer" />
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
-          Login
-        </button>
+        {!user ? (
+          <button
+            onClick={OpenSignIn}
+            className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton />
+        )}
       </div>
       <MenuIcon
-      onClick={()=> setIsOpen(!isOpen)}
-      className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" />
+        onClick={() => setIsOpen(!isOpen)}
+        className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer"
+      />
     </div>
   );
 };
